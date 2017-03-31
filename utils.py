b8e5151e83e7f0669b88e1c7251439db289f7323
Tiago Batalhão <tiagobbatalhao@gmail.com>
2017-03-31 11:18:16 +0800

    Added gates (Hadamard and CNOT)

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ Basic functions for Quantum Information Processing
 
 import pylab as py
 import qutip as qp
-import itertools,os,sys
+import itertools,os,sys,cmath
 
 _pauliBasis = {}
 _pauliBasis[1] = {}
@@ -47,26 +47,33 @@ def combineBasis(dicBasisA,dicBasisB):
 			newBasis[label] = op
 	return newBasis
 
-def getBlochForm(operator,arrayForm=False,realPartOnly=False):
+def getBlochForm(operator,filterResults=True,arrayForm=False,realPartOnly=False):
 	"""
 	Get a representation of an operator in terms of a Bloch vector
 	"""
+	threshold = 1e-12
 	nQubits = getNumberOfQubits(operator)
 	basis = getPauliBasis(nQubits)
-	bloch = {}
 	normalization = 2**(-nQubits)
-	for label,op in basis.items():
-		expect = qp.expect(op,operator)
-		if realPartOnly:
-			expect = expect.real
-		bloch[label] = expect * normalization
 	if not arrayForm:
+		bloch = {}
+		for label,op in basis.items():
+			expect = qp.expect(op,operator)
+			if realPartOnly:
+				expect = expect.real
+			if (not filterResults) or (abs(expect) > threshold):
+				bloch[label] = expect * normalization
 		return bloch
 	else:
 		# Return in a vector format
+		array = []
 		keys = bloch.keys()
 		keys.sort()
-		array = [bloch[x] for x in keys]
+		for key in keys:
+			expect = qp.expect(basis[key],operator)
+			if realPartOnly:
+				expect = expect.real
+			array.append(expect * normalization)
 		return array
 
 def getOperatorForm(bloch):
@@ -109,14 +116,14 @@ def getNumberOfQubits(operator):
 	return nQubits
 
 
-def getUnitaryToNormalForm(unitary):
+def convertUnitaryToNormalForm(unitary):
 	"""
 	Multiply an unitary by a phase factor so that it has real trace.
 	If trace is zero, consider the Pauli expansion.
 	"""
 	threshold = 1e-12
 	nQubits = getNumberOfQubits(unitary)
-	for lab,op in getPauliBasis(nQubits):
+	for lab,op in getPauliBasis(nQubits).items():
 		trace = (op * unitary).tr()
 		if abs(trace) > threshold:
 			phase = cmath.phase(trace)
@@ -124,9 +131,65 @@ def getUnitaryToNormalForm(unitary):
 	return py.exp(-1j*phase) * unitary
 
 
-def gitcommit(message=None):
+def getSingleQubitRotation(angle,axis):
+	"""
+	Return an unitary operator defined on a single qubit
+	"""
+	ketZ = [qp.basis(2,x) for x in range(2)]
+	if axis.lower() in ['z','+z']:
+		kets = ketZ
+	elif axis.lower() in ['x','+x']:
+		kets = [(ketZ[0]+x*ketZ[1]).unit() for x in [+1,-1]]
+	elif axis.lower() in ['y','+y']:
+		kets = [(ketZ[0]+x*ketZ[1]).unit() for x in [+1j,-1j]]
+	elif axis.lower() in ['-z']:
+		kets = ketZ[::-1]
+	elif axis.lower() in ['-x']:
+		kets = [(ketZ[0]+x*ketZ[1]).unit() for x in [-1,+1]]
+	elif axis.lower() in ['-y']:
+		kets = [(ketZ[0]+x*ketZ[1]).unit() for x in [-1j,+1j]]
+	unitary = qp.ket2dm(kets[0]) + py.exp(-1j*angle) * qp.ket2dm(kets[1])
+	return unitary
+
+def getUnitaryRotation(angle,axis):
+	"""
+	Return an unitary operator defined on multiple qubits
+	"""
+	unitaries = [getSingleQubitRotation(x,y) for x,y in zip(angle,axis)]
+	unitary = reduce(lambda old,new: new * old,unitaries)
+	return unitary
+
+def getControlledPauli(nQubits=2,bits=[0,1],basis='zz'):
+	"""
+	Return the controlled gate in Pauli basis between bits
+	"""
+	idList = [qp.qeye(2)] * nQubits
+	sigA = _pauliBasis[1][basis[0].upper()]
+	sigB = _pauliBasis[1][basis[1].upper()]
+
+	II = qp.tensor(idList)
+	opList = idList[:]
+	opList[bits[0]] = sigA
+	SI = qp.tensor(opList)
+	opList = idList[:]
+	opList[bits[1]] = sigB
+	IS = qp.tensor(opList)
+	opList = idList[:]
+	opList[bits[0]] = sigA
+	opList[bits[1]] = sigB
+	SS = qp.tensor(opList)
+	operator = (II + SI + IS - SS) / 2.0
+	return operator
+
+def getHadamardGate(nQubits=1):
+	"""
+	Return the Hadamard gate on multiple qubits
+	"""
+	return qp.tensor([qp.snot()] * nQubits)
+
+
+
+def _gitcommit(message=None):
 	os.system('git add ' + __file__)
 	os.system('git commit -m "' + ('' if message is None else str(message)) + '"')
 	os.system('git push origin master')
-
-print 'One more test'
